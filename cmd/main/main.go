6ecf638f826fd2b6c0ffbf55145d661d116ee4d4
Shubham Gupta <gupta.shubham97@gmail.com>
2024-03-28 19:09:56 +0530

    add two more routes and run server

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/shubhamgupta97/go-jwt-project/pkg/routes"
 )
 
 func main() {
@@ -15,5 +18,18 @@ func main() {
 
 	router := gin.New()
 	router.Use(gin.Logger())
+
+	routes.AuthRoutes(router)
+	routes.UserRoutes(router)
+
+	router.GET("/api-1", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{"success": "Access granted for api-1"})
+	})
+
+	router.POST("/api-2", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{"success": "Access granted for api-2"})
+	})
+
+	router.Run(fmt.Sprintf(":%s", port))
 
 }
